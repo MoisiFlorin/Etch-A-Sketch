6f1009895ac2d5d78f9ecb62cacd7e32ddcd3451
Florin <moisi.florin13@gmail.com>
2018-07-05 20:16:55 +0300

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function createGrid(gridSize) {
+    document.styleSheets[0].cssRules[3].style["grid-template-columns"] = "repeat(" + gridSize  + ", 1fr)"
+    document.styleSheets[0].cssRules[3].style["grid-template-rows"] = "repeat(" + gridSize  + ", 1fr)"
+    
     for (let i = 0; i < (gridSize * gridSize); i++) {
         let square = document.createElement("div");
         square.className = 'square';
@@ -9,7 +12,7 @@ function createGrid(gridSize) {
 
 createGrid(16);
 
-function colorSquare() {
+function blackSquare() {
     let squares = Array.from(document.querySelectorAll(".square"));
     for (i = 0; i < squares.length; i++) {
         squares[i].addEventListener('mouseover', function (e) {
@@ -18,7 +21,20 @@ function colorSquare() {
     }
 }
 
-colorSquare();
+blackSquare();
+
+/*
+function colorSquare() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+*/
+
+
 
 document.querySelector("#reset").addEventListener('click', e => {
     let newGrid = Number(window.prompt('Enter a new grid size', 16));
@@ -27,6 +43,21 @@ document.querySelector("#reset").addEventListener('click', e => {
             container.removeChild(container.lastChild);
         }
         createGrid(newGrid);
-        colorSquare();
+        blackSquare();
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
